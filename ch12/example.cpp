a951e5203e95cbfbafddb4ce9153ac91afa49056
Meng Fanze <ifanze@outlook.com>
2018-06-06 22:52:31 +0800

    update

--- ch12/example.cpp
+++ ch12/example.cpp
@@ -1,72 +1,4 @@
-#include <string>
-#include <vector>
-#include <memory>
-#include <iostream>
-#include <fstream>
-
-using namespace std;
-
-class StrBlobPtr;
-
-class StrBlob
-{
-    friend class StrBlobPtr;
-    friend class ConstStrBlobPtr;
-
-  public:
-    typedef vector<string>::size_type size_type;
-
-    StrBlob() : data(make_shared<vector<string>>()) {}
-    StrBlob(std::initializer_list<string> li) : data(make_shared<vector<string>>(li)) {}
-
-    size_type size() const { return data->size(); }
-    bool empty() const { return data->empty(); }
-    void push_back(const string &t) { data->push_back(t); }
-
-    string &front();
-    string &back();
-    const string &const_front() const;
-    const string &const_back() const;
-    void pop_back();
-
-    StrBlobPtr begin();
-    StrBlobPtr end();
-
-  private:
-    shared_ptr<vector<string>> data;
-    void check(size_type i, const string &msg) const;
-};
-
-class StrBlobPtr
-{
-  public:
-    StrBlobPtr() : curr(0) {}
-    StrBlobPtr(StrBlob &a, size_t sz = 0) : wptr(a.data), curr(sz) {}
-    string& deref() const;
-    StrBlobPtr &incr();
-
-  private:
-    shared_ptr<vector<string>> check(std::size_t, const std::string &) const;
-    weak_ptr<vector<string>> wptr;
-    size_t curr;
-};
-
-class ConstStrBlobPtr
-{
-  public:
-    ConstStrBlobPtr() : curr(0) {}
-    ConstStrBlobPtr(const StrBlob &a, size_t sz = 0) : wptr(a.data), curr(sz) {}
-    const string& deref() const;
-    ConstStrBlobPtr &incr();
-
-  private:
-    shared_ptr<vector<string>> check(std::size_t, const std::string &) const;
-    weak_ptr<vector<string>> wptr;
-    size_t curr;
-};
-
-//////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////
+#include "example.h"
 
 shared_ptr<vector<string>>
 ConstStrBlobPtr::check(size_t i, const string &msg) const
@@ -210,8 +142,8 @@ void test2()
     }
 }
 
-int main()
-{
-    test1();
-    test2();
-}
+// int main()
+// {
+//     test1();
+//     // test2();
+// }
--- ch12/execise_12.32.cpp
+++ ch12/execise_12.32.cpp
@@ -7,22 +7,24 @@
 #include <sstream>
 #include <fstream>
 
+#include "example.h"
+
 using namespace std;
 
 class QueryResult;
 class TextQuery
 {
   public:
-    using line_no = vector<string>::size_type;
+    using line_no = StrBlob::size_type;
     TextQuery(ifstream &);
     QueryResult query(const string &) const;
 
   private:
-    shared_ptr<vector<string>> file;
+    shared_ptr<StrBlob> file;
     map<string, shared_ptr<set<line_no>>> wm;
 };
 
-TextQuery::TextQuery(ifstream &is) : file(new vector<string>)
+TextQuery::TextQuery(ifstream &is) : file(new StrBlob)
 {
     string text;
     while (getline(is, text))
@@ -46,19 +48,19 @@ class QueryResult
     friend ostream &print(ostream &, const QueryResult &);
 
   public:
-    using line_no = vector<string>::size_type;
+    using line_no = StrBlob::size_type;
     using ResultIter = set<line_no>::iterator;
 
-    QueryResult(string s, shared_ptr<set<line_no>> p, shared_ptr<vector<string>> f) : sought(s), lines(p), file(f) {}
-    
+    QueryResult(string s, shared_ptr<set<line_no>> p, shared_ptr<StrBlob> f) : sought(s), lines(p), file(f) {}
+
     ResultIter begin() { return lines->begin(); }
     ResultIter end() { return lines->end(); }
-    shared_ptr<vector<string>> get_file() { return file; }
+    shared_ptr<StrBlob> get_file() { return file; }
 
   private:
     string sought;
     shared_ptr<set<line_no>> lines;
-    shared_ptr<vector<string>> file;
+    shared_ptr<StrBlob> file;
 };
 
 void runQueries(ifstream &infile)
@@ -88,7 +90,7 @@ ostream &print(ostream &os, const QueryResult &qr)
 {
     os << qr.sought << " occurs " << qr.lines->size() << " times" << endl;
     for (auto num : *qr.lines)
-        os << "\t(line " << num + 1 << ")" << *(qr.file->begin() + num) << endl;
+        os << "\t(line " << num + 1 << ")" << (ConstStrBlobPtr(*qr.file, num)).deref() << endl;
     return os;
 }
 
